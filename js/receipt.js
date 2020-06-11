7743a4580e10ed037f6dbe4baddd683d96a9420e
jefftran <jefftran@iu.edu>
2020-06-10 20:31:52 -0400

    modal image

--- js/receipt.js
+++ js/receipt.js
@@ -107,8 +107,8 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
   
   // When the modal is shown, we want a fixed body
-  document.body.style.position = 'fixed';
-  document.body.style.top = `-${window.scrollY}px`;
+  // document.body.style.position = 'fixed';
+  // document.body.style.top = `-${window.scrollY}px`;
 }
 
 // Get the <span> element that closes the modal
@@ -119,8 +119,8 @@ span.onclick = function() {
   modal.style.display = "none";
 
   // When the modal is hidden...
-  const scrollY = document.body.style.top;
-  document.body.style.position = '';
-  document.body.style.top = '';
-  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+  // const scrollY = document.body.style.top;
+  // document.body.style.position = '';
+  // document.body.style.top = '';
+  // window.scrollTo(0, parseInt(scrollY || '0') * -1);
 }
