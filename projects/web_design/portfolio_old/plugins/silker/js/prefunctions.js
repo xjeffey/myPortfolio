81343029fb948ea49419f844c333ee14d9f4f234
xjeffey <tranjeff@live.com>
2021-01-13 21:27:47 -0500

    test

--- projects/web_design/portfolio_old/plugins/silker/js/prefunctions.js
+++
@@ -1,4 +0,0 @@
-function createExampleBlockById(numberId) {
-	var source = $(".example-" + numberId);
-	$('<pre class="code-example-' + numberId + '">').insertAfter($('.example-' + numberId)).text(source.html());
-}
