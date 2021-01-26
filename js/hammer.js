f0ef1761dae2543a4043bf2fe95a6f5dd7c9334d
xjeffey <tranjeff@live.com>
2021-01-26 01:15:38 -0500

    navigation hamburger

--- js/hammer.js
+++ js/hammer.js
@@ -32,6 +32,13 @@ let closeBtn = document.querySelector(".closebtn");
 // the hamburger links 
 let hamburgerLinks = document.querySelectorAll(".overlay-content a");
 
+// drop down overlay
+let dropDownButton = document.getElementById("projects-button");
+//drop down links
+let dropDownLinks = document.querySelector(".overlay-dropdown-content");
+//flag variable
+let dropPressed = false;
+
 /* Open when someone clicks on the span element */
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
@@ -51,6 +58,10 @@ function closeNav() {
   document.body.style.position = '';
   document.body.style.top = '';
   window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+  // resetting drop down 
+  dropPressed = false;
+  dropDownLinks.style.display = "none";
 }
 
 // adding the functions to the buttons
@@ -61,3 +72,16 @@ closeBtn.addEventListener('click', closeNav);
 for(let link of hamburgerLinks){
   link.addEventListener('click', closeNav);
 }
+
+// open close drop down in nav 
+function openCloseDropDown(){
+  if (dropPressed){
+    dropPressed = false;
+    dropDownLinks.style.display = "none";
+  }else{
+    dropPressed = true;
+    dropDownLinks.style.display = "block";
+  }
+}
+
+dropDownButton.addEventListener('click', openCloseDropDown);
